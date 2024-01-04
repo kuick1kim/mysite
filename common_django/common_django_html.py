f162a96b9f5d250f2ce00f722a0212b2a5b5f7ff
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2024-01-05 07:42:58 +0900

    추가변경

--- common_django/common_django_html.py
+++ common_django/common_django_html.py
@@ -150,80 +150,75 @@
 
 ###############################################################
 ############### 아래것은 붙여서 사용하면 된다. ###################
+########## html 변경하는 것 그냥 그 폴더에서 실행하면됨 ##########
 ###############################################################
 ###############################################################
 
+import os
+import re
 
-
-
-
-
-
-# import os
-# import re
-
-# def findPatterns(str):
-#     pattern = r'(([\'|"])(css|js|demo-images|fonts|images)\/.*\.(css|js|png|jpg|gif)([\'|"]))'
+def findPatterns(str):
+    pattern = r'(([\'|"])(css|js|demo-images|fonts|images)\/.*\.(css|js|png|jpg|gif)([\'|"]))'
     
-#     p = re.compile(pattern)
-#     results = p.findall(str)
+    p = re.compile(pattern)
+    results = p.findall(str)
     
-#     return results
+    return results
 
-# #다음으로는 이 경로를 장고 경로로 바꿔줘야 하지.
-# #"css/~~.css" -> "{% static '/css/~~.css' %}"
-# #일단 "나 '로 시작하는지 판단하고
-# #그 문자를 앞뒤에서 제거한다.
+#다음으로는 이 경로를 장고 경로로 바꿔줘야 하지.
+#"css/~~.css" -> "{% static '/css/~~.css' %}"
+#일단 "나 '로 시작하는지 판단하고
+#그 문자를 앞뒤에서 제거한다.
 
-# def transformLink(link):
-#     curQuote = None
-#     quote1 = "'"
-#     quote2 = '"'
+def transformLink(link):
+    curQuote = None
+    quote1 = "'"
+    quote2 = '"'
 
-#     if link.startswith(quote1):
-#         _link = link.strip(quote1)
-#         curQuote = quote1
-#     else:
-#         _link = link.strip(quote2)
-#         curQuote = quote2
+    if link.startswith(quote1):
+        _link = link.strip(quote1)
+        curQuote = quote1
+    else:
+        _link = link.strip(quote2)
+        curQuote = quote2
     
-# ##    if not _link.startswith("/"):
-# ##        _link = f"/{_link}"
+##    if not _link.startswith("/"):
+##        _link = f"/{_link}"
 
-#     if curQuote == quote1:
-#         newLink = f'{{% static "{_link}" %}}'
-#         newLink = quote1 + newLink + quote1
-#     else:
-#         newLink = f"{{% static '{_link}' %}}"
-#         newLink = quote2 + newLink + quote2
-#     return newLink
+    if curQuote == quote1:
+        newLink = f'{{% static "{_link}" %}}'
+        newLink = quote1 + newLink + quote1
+    else:
+        newLink = f"{{% static '{_link}' %}}"
+        newLink = quote2 + newLink + quote2
+    return newLink
 
-# currentPath = os.path.dirname(os.path.abspath(__file__))
+currentPath = os.path.dirname(os.path.abspath(__file__))
 
-# #os.walk를 이용해서 모든 하위 폴더의 파일까지 순차적으로 접근
-# for root, dirs, files in os.walk(currentPath):
-#     for file in files:
-#         #파일을 하나씩 접근해서 .cs로 끝나지 않는 파일은 제외
-#         filePath = os.path.join(root, file)
-#         if not filePath.endswith(".html"):
-#             continue
+#os.walk를 이용해서 모든 하위 폴더의 파일까지 순차적으로 접근
+for root, dirs, files in os.walk(currentPath):
+    for file in files:
+        #파일을 하나씩 접근해서 .cs로 끝나지 않는 파일은 제외
+        filePath = os.path.join(root, file)
+        if not filePath.endswith(".html"):
+            continue
         
-#         print(filePath)
-#         #파일에서 내용 읽어오기
-#         with open(filePath, 'r', encoding='utf8') as file :
-#             filedata = file.read()
+        print(filePath)
+        #파일에서 내용 읽어오기
+        with open(filePath, 'r', encoding='utf8') as file :
+            filedata = file.read()
 
-#         with open(filePath, 'w', encoding='utf8') as file:
-#             file.write("{% load static %}\n")
+        with open(filePath, 'w', encoding='utf8') as file:
+            file.write("{% load static %}\n")
 
-#         results = findPatterns(filedata)
-#         print(results)
-#         for result in results:
-#             transformed = transformLink(result[0])
-#             print(f"from : {result[0]} -> {transformed}")
-#             filedata = filedata.replace(result[0], transformed)
+        results = findPatterns(filedata)
+        print(results)
+        for result in results:
+            transformed = transformLink(result[0])
+            print(f"from : {result[0]} -> {transformed}")
+            filedata = filedata.replace(result[0], transformed)
         
             
-#         #바꾼 내용을 파일에 다시 기록
-#         with open(filePath, 'a', encoding='utf8') as file:
-#             file.write(filedata)
+        #바꾼 내용을 파일에 다시 기록
+        with open(filePath, 'a', encoding='utf8') as file:
+            file.write(filedata)
